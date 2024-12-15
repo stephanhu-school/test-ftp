8f79ff8ad591898bdea785474ba87fbdab9d3eb3
Stephanvr99 <stephanvanravenzwaaij99@live.nl>
2024-12-15 15:11:05 +0100

    ftp script v2

--- install-ftp.sh
+++ install-ftp.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 # Variabelen uit parameters
-ADMIN_USER=$1
-FTPUSER1_PASSWORD=$2
-FTPUSER2_PASSWORD=$3
-FTP_DATA_DIR=$4
+FTPUSER1_PASSWORD=$1
+FTPUSER2_PASSWORD=$2
+FTP_DATA_DIR=$3
+ADMIN_USER=$4
 FTP_GROUP="ftpusers"
 
 # Updates uitvoeren en vsftpd installeren
 apt-get update -y
-apt-get upgrade -y
 apt-get install -y vsftpd
 apt-get install -y ufw
 
-# firewall allow ssh and ftp
-ufw allow ssh
-ufw allow ftp
-ufw enable
+# Gedeelde directory aanmaken
+mkdir -p $FTP_DATA_DIR
+chmod 770 $FTP_DATA_DIR
 
-# Configuratie van vsftpd
+# Groep aanmaken en directory aan groep koppelen
+groupadd $FTP_GROUP
+chown :$FTP_GROUP $FTP_DATA_DIR
+
+# FTP-gebruikers aanmaken en aan de groep toevoegen
+useradd -m -d $FTP_DATA_DIR -s /sbin/nologin -G $FTP_GROUP ftpuser1
+echo "ftpuser1:$FTPUSER1_PASSWORD" | chpasswd
+useradd -m -d $FTP_DATA_DIR -s /sbin/nologin -G $FTP_GROUP ftpuser2
+echo "ftpuser2:$FTPUSER2_PASSWORD" | chpasswd
+
+# Admin-gebruiker toevoegen aan de FTP-groep
+usermod -a -G $FTP_GROUP $ADMIN_USER
+
+# Configuratie van vsftpd aanpassen
 cat <<EOL > /etc/vsftpd.conf
 listen=YES
 anonymous_enable=NO
@@ -27,28 +38,28 @@ write_enable=YES
 local_umask=022
 chroot_local_user=YES
 allow_writeable_chroot=YES
+user_sub_token=\$USER
+local_root=$FTP_DATA_DIR
 pasv_enable=YES
 pasv_min_port=1024
 pasv_max_port=1048
 EOL
 
-# Gedeelde directory aanmaken
-mkdir -p $FTP_DATA_DIR
-chmod 755 $FTP_DATA_DIR
+# Beveiligingsmaatregelen
+echo "ftpuser1" | tee -a /etc/vsftpd.userlist
+echo "ftpuser2" | tee -a /etc/vsftpd.userlist
+echo $ADMIN_USER | tee -a /etc/vsftpd.userlist
+echo "userlist_enable=YES" | tee -a /etc/vsftpd.conf
+echo "userlist_file=/etc/vsftpd.userlist" | tee -a /etc/vsftpd.conf
+echo "userlist_deny=NO" | tee -a /etc/vsftpd.conf
 
-# FTP-groep aanmaken
-groupadd $FTP_GROUP
-chown :$FTP_GROUP $FTP_DATA_DIR
-chmod 770 $FTP_DATA_DIR
-
-# FTP-gebruikers aanmaken en toevoegen aan de groep
-useradd -m -d $FTP_DATA_DIR -s /sbin/nologin -G $FTP_GROUP ftpuser1
-echo "ftpuser1:$FTPUSER1_PASSWORD" | chpasswd
-useradd -m -d $FTP_DATA_DIR -s /sbin/nologin -G $FTP_GROUP ftpuser2
-echo "ftpuser2:$FTPUSER2_PASSWORD" | chpasswd
-
-# FTP-server opnieuw starten
+# Service herstarten
 systemctl restart vsftpd
 systemctl enable vsftpd
 
-echo "FTP-server installatie voltooid. Gebruikers hebben toegang tot $FTP_DATA_DIR via groep $FTP_GROUP."
+# firewall allow ssh and ftp
+ufw allow ssh
+ufw allow ftp
+ufw enable
+
+echo "FTP-server configuratie voltooid. Alleen toegang tot $FTP_DATA_DIR toegestaan."
